dfc72153cd31ebc74e66d443fa314edd8882ec4c
Bianca Besciu <bianca.besciu@gmail.com>
2018-06-29 16:01:15 +0200

    function alternateRow checks for odd or even rows; add another row when pressing add button

--- main.js
+++ main.js
@@ -1,13 +1,8 @@
-let pet = document.getElementsByClassName('pet');
-let name = document.getElementsByClassName('name');
-let color = document.getElementsByClassName('color');
-let gender = document.getElementsByClassName('gender');
-
 const Obj = {
-    'pet': pet,
-    'name': name,
-    'color': color,
-    'gender': gender
+    'pet': "",
+    'name': "",
+    'color': "",
+    'gender': ""
 };
 
 let tr = document.getElementsByClassName('row');
@@ -24,5 +19,45 @@ function alternateRow() {
         }
     }
 }
+
+let addRowBtn = document.getElementById('addRow');
+addRowBtn.addEventListener('click', function() {
+    let pet = document.getElementById('pet').value;
+    let name = document.getElementById('name').value;
+    let color = document.getElementById('color').value;
+    let gender = document.getElementById('gender').value;
+    let table = document.getElementById('tableBody');
+
+    Obj.pet = pet;
+    Obj.name = name;
+    Obj.color = color;
+    Obj.gender = gender;
+
+    let newRow = document.createElement('tr');
+    newRow.className = "row";
+    let td1 = document.createElement('td');
+    let td2 = document.createElement('td');
+    let td3 = document.createElement('td');
+    let td4 = document.createElement('td');
+    let td5 = document.createElement('td');
+    let btnDelete = document.createElement('input');
+    btnDelete.type = "button";
+    btnDelete.value = "Delete";
+    td5.appendChild(btnDelete);
+
+    td1.innerHTML = Obj.pet;
+    td2.innerHTML = Obj.name;
+    td3.innerHTML = Obj.color;
+    td4.innerHTML = Obj.gender;
+
+    newRow.appendChild(td1);
+    newRow.appendChild(td2);
+    newRow.appendChild(td3);
+    newRow.appendChild(td4);
+    newRow.appendChild(td5);
+
+    table.appendChild(newRow);
+    alternateRow(table.children)
+});
 
 document.addEventListener('DOMContentLoaded', alternateRow);
